718674663a755402a5616de9dad49f3a87eb5ecb
Naf <nnegussie7@gmail.com>
2022-10-04 00:17:22 -0400

    current date display

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,11 @@
+//display current date
+let currentDate = document.getElementById("current-day");
+currentDate.textContent = moment().format("LLLL");
 
 
+//save task
+	$('.saveBtn').button(function(click){
 
-// mobiscroll.setOptions({
-//     theme: 'ios',
-//     themeVariant: 'light',
-//     clickToCreate: true,
-//     dragToCreate: true,
-//     dragToMove: true,
-//     dragToResize: true,
-//     eventDelete: true
-// });
+	});
 
-// $(function () {
 
-//     var inst = $('#demo-desktop-day-view').mobiscroll().eventcalendar({
-//         view: {
-//             schedule: { type: 'day' }
-//         },
-//         onEventClick: function (event, inst) {
-//             mobiscroll.toast({
-//                 message: event.event.title
-//             });
-//         }
-//     }).mobiscroll('getInst');
-
-//     $.getJSON('https://trial.mobiscroll.com/events/?vers=5&callback=?', function (events) {
-//         inst.setEvents(events);
-//     }, 'jsonp');
-
-// });
